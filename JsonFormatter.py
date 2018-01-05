92ebb73d68b4f86905ec8f4ad6cf91e527066293
Alexey Litvinov <sullenor@yandex-team.ru>
2018-01-05 10:50:10 +0300

    add a better error handling

--- JsonFormatter.py
+++ JsonFormatter.py
@@ -2,61 +2,67 @@ import json
 import sublime
 import sublime_plugin
 
-def get_regions(view):
-  regions = tuple(filter(lambda r: not r.empty(), view.sel()))
+class JsonFormatterUtils:
+  @staticmethod
+  def parse_json(string):
+    return json.loads(string)
 
-  if len(regions) > 0:
-    return regions
+  @staticmethod
+  def serialize_json(structure, indent=None, item_separator=',', key_separator=':', sort_keys=False):
+    return json.dumps(structure, ensure_ascii=False, indent=indent, separators=(item_separator, key_separator), sort_keys=False)
 
-  return (sublime.Region(0, view.size()),)
+  def parse_region(self, region):
+    string = self.view.substr(region)
 
-def parse_json(json_string):
-  try:
-    return json.loads(json_string)
-  except ValueError:
-    return None
+    try:
+      return (self.parse_json(string), region)
+    except ValueError as err:
+      raise err
 
-def serialize_json(structure, indent=None, item_separator=',', key_separator=':', sort_keys=False):
-  return json.dumps(structure, ensure_ascii=False, indent=indent, separators=(item_separator, key_separator), sort_keys=False)
+  def regions(self):
+    regions = [region for region in self.view.sel() if not region.empty()]
 
-class MinifyJsonCommand(sublime_plugin.TextCommand):
+    if len(regions) > 0:
+      return regions
+
+    # create a region from the contents of file in case all regions are empty
+    return [sublime.Region(0, self.view.size())]
+
+
+class MinifyJsonCommand(JsonFormatterUtils, sublime_plugin.TextCommand):
   def run(self, edit):
-    regions = reversed(get_regions(self.view))
+    try:
+      structures = tuple(map(self.parse_region, self.regions()))
 
-    for region in regions:
-      json_string = self.view.substr(region)
-      structure = parse_json(json_string)
+      for structure, region in tuple(reversed(structures)):
+        string = self.serialize_json(structure)
+        self.view.replace(edit, region, string)
 
-      if not structure:
-        self.view.window().status_message('Invalid json')
-      else:
-        minified_json_string = serialize_json(structure)
-        self.view.replace(edit, region, minified_json_string)
+    except ValueError as err:
+      sublime.status_message(str(err))
 
-class PrettifyJsonCommand(sublime_plugin.TextCommand):
+
+class PrettifyJsonCommand(JsonFormatterUtils, sublime_plugin.TextCommand):
   def run(self, edit):
-    regions = reversed(get_regions(self.view))
+    try:
+      structures = tuple(map(self.parse_region, self.regions()))
 
-    for region in regions:
-      json_string = self.view.substr(region)
-      structure = parse_json(json_string)
+      for structure, region in tuple(reversed(structures)):
+        string = self.serialize_json(structure, indent=2, item_separator=', ', key_separator=': ')
+        self.view.replace(edit, region, string)
 
-      if not structure:
-        self.view.window().status_message('Invalid json')
-      else:
-        minified_json_string = serialize_json(structure, indent=2, item_separator=', ', key_separator=': ')
-        self.view.replace(edit, region, minified_json_string)
+    except ValueError as err:
+      sublime.status_message(str(err))
 
-class PrettifyJsonAndSortKeysCommand(sublime_plugin.TextCommand):
+
+class PrettifyJsonAndSortKeysCommand(JsonFormatterUtils, sublime_plugin.TextCommand):
   def run(self, edit):
-    regions = reversed(get_regions(self.view))
+    try:
+      structures = tuple(map(self.parse_region, self.regions()))
 
-    for region in regions:
-      json_string = self.view.substr(region)
-      structure = parse_json(json_string)
+      for structure, region in tuple(reversed(structures)):
+        string = self.serialize_json(structure, indent=2, item_separator=', ', key_separator=': ', sort_keys=True)
+        self.view.replace(edit, region, string)
 
-      if not structure:
-        self.view.window().status_message('Invalid json')
-      else:
-        minified_json_string = serialize_json(structure, indent=2, item_separator=', ', key_separator=': ', sort_keys=True)
-        self.view.replace(edit, region, minified_json_string)
+    except ValueError as err:
+      sublime.status_message(str(err))
