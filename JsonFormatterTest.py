3ddfde4b8dda5879266cc15e212c6192b59fa3d7
Alexey Litvinov <sullenor@yandex-team.ru>
2018-01-06 13:50:51 +0300

    fix tests

--- JsonFormatterTest.py
+++ JsonFormatterTest.py
@@ -6,24 +6,25 @@ import sublime_plugin_lib_mock
 sys.modules['sublime'] = sublime_lib_mock
 sys.modules['sublime_plugin'] = sublime_plugin_lib_mock
 
-from JsonFormatter import parse_json, serialize_json
+from JsonFormatter import JsonFormatterUtils
 
 class UtilsTests(unittest.TestCase):
   def test_cyrillic_parsing(self):
-    structure = parse_json('{"cyrillic":"Дизайн системы"}')
+    structure = JsonFormatterUtils.parse_json('{"cyrillic":"Дизайн системы"}')
     self.assertEqual(structure, {'cyrillic': 'Дизайн системы'})
 
   def test_cyrillic_stringifying(self):
-    string = serialize_json({'cyrillic': 'Дизайн системы'})
+    string = JsonFormatterUtils.serialize_json({'cyrillic': 'Дизайн системы'})
     self.assertEqual(string, '{"cyrillic":"Дизайн системы"}')
 
   def test_null_value_parsing(self):
-    structure = parse_json('{"null":null}')
+    structure = JsonFormatterUtils.parse_json('{"null":null}')
     self.assertEqual(structure, {'null': None})
 
   def test_null_value_stringifying(self):
-    string = serialize_json({'null': None})
+    string = JsonFormatterUtils.serialize_json({'null': None})
     self.assertEqual(string, '{"null":null}')
 
+
 if __name__ == '__main__':
-  UtilsTests.main()
+  unittest.main()
